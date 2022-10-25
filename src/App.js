b62a62b66ea7e50c7fbb8c084faa8f54f1daf236
igknighton <trent.knighton@gmail.com>
2022-10-24 21:21:44 -0500

    Calculation implementation

--- src/App.js
+++ src/App.js
@@ -20,12 +20,67 @@ import {themes} from "./styles/colors";
 function App() {
 
     const [themeOption,setThemeOption] = useState(1);
+    const [numberInput,setNumberInput] = useState('');
+    const [num1,setNum1] = useState(0);
+    const [operator,setOperator] = useState('');
 
+    const clearNumberState = () => {
+        setNum1(0);
+        setNumberInput('');
+        setOperator('');
+    }
+
+    const handleOperator = operator => {
+        setOperator(operator);
+        setNum1(parseFloat(numberInput));
+        setNumberInput('')
+    }
+
+    const handleCalculation = calculation => {
+        setNumberInput(calculation)
+        setOperator('=')
+    }
+
+    const calculate = () => {
+        const n1 = parseFloat(num1);
+        const n2 = parseFloat(numberInput)
+        switch (operator) {
+            case '+':
+                handleCalculation(`${n1+n2}`);
+                break;
+            case '-':
+                handleCalculation(`${n1-n2}`);
+                break;
+            case '*':
+                handleCalculation(`${n1*n2}`);
+                break;
+            case '/':
+                handleCalculation(`${n1/n2}`);
+                break;
+            case '':
+                break;
+            default:
+                throw new Error('Unknown Operator')
+        }
+    }
 
     const handleToggle = () => {
         if (themeOption === 3) setThemeOption(1)
         else setThemeOption(themeOption+1)
     }
+
+    const handleCalcInput = number => {
+        if (operator === '=') {
+            setNum1(0);
+            setOperator('');
+            setNumberInput(number);
+        }
+        else {
+            setNumberInput(numberInput.concat(number))
+        }
+    }
+
+    const deleteNumber = () => setNumberInput(numberInput.slice(0,-1))
 
     const ThemeToggler = () => {
         return <TogglerContainer>
@@ -37,9 +92,9 @@ function App() {
     }
 
     const CalculatorInput = () => {
-
         return <NumberInput>
-            123,456
+            <div>{operator}</div>
+            {numberInput}
         </NumberInput>
     }
 
@@ -63,28 +118,28 @@ function App() {
 
         return <ButtonContainer>
 
-            <FullButton>7</FullButton>
-            <FullButton>8</FullButton>
-            <FullButton>9</FullButton>
-            <FullButton operation>DEL</FullButton>
+            <FullButton onClick={() => handleCalcInput('7')}>7</FullButton>
+            <FullButton onClick={() => handleCalcInput('8')}>8</FullButton>
+            <FullButton onClick={() => handleCalcInput('9')}>9</FullButton>
+            <FullButton onClick={deleteNumber} operation>DEL</FullButton>
 
-            <FullButton>4</FullButton>
-            <FullButton>5</FullButton>
-            <FullButton>6</FullButton>
-            <FullButton>+</FullButton>
+            <FullButton onClick={() => handleCalcInput('4')}>4</FullButton>
+            <FullButton onClick={() => handleCalcInput('5')}>5</FullButton>
+            <FullButton onClick={() => handleCalcInput('6')}>6</FullButton>
+            <FullButton onClick={() => handleOperator('+')}>+</FullButton>
 
-            <FullButton>1</FullButton>
-            <FullButton>2</FullButton>
-            <FullButton>3</FullButton>
-            <FullButton>-</FullButton>
+            <FullButton onClick={() => handleCalcInput('1')}>1</FullButton>
+            <FullButton onClick={() => handleCalcInput('2')}>2</FullButton>
+            <FullButton onClick={() => handleCalcInput('3')}>3</FullButton>
+            <FullButton onClick={() => handleOperator('-')} >-</FullButton>
 
-            <FullButton>.</FullButton>
-            <FullButton>0</FullButton>
-            <FullButton>/</FullButton>
-            <FullButton>X</FullButton>
+            <FullButton onClick={() => handleCalcInput('.')}>.</FullButton>
+            <FullButton onClick={() => handleCalcInput('0')}>0</FullButton>
+            <FullButton onClick={() => handleOperator('/')}>/</FullButton>
+            <FullButton onClick={() => handleOperator('*')}>X</FullButton>
 
-            <FullButton size={'lg'} operation>RESET</FullButton>
-            <FullButton size={'lg'} eq>=</FullButton>
+            <FullButton size={'lg'} onClick={clearNumberState} operation>RESET</FullButton>
+            <FullButton size={'lg'} onClick={calculate} eq>=</FullButton>
         </ButtonContainer>
     }
 
